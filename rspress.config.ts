16549b0b0f007587cd72bbb192bd199e46eafca4
jxiansen <3032825994@qq.com>
2023-10-15 10:23:42 +0800

    增加掘金文章

--- rspress.config.ts
+++ rspress.config.ts
@@ -91,6 +91,72 @@ function getSidebarConf() {
       ],
     },
     {
+      text: "设计模式",
+      collapsible: true,
+      items: [
+        { text: "开篇：前端工程师的成长论", link: "/designPattern/1" },
+        { text: "设计模式的“道”与“术”", link: "/designPattern/2" },
+        {
+          text: "创建型：工厂模式·简单工厂——区分“变与不变”",
+          link: "/designPattern/3",
+        },
+        {
+          text: "创建型：工厂模式·抽象工厂——理解“开放封闭”",
+          link: "/designPattern/4",
+        },
+        {
+          text: "创建型：单例模式——Vuex的数据管理哲学",
+          link: "/designPattern/5",
+        },
+        {
+          text: "创建型：单例模式——面试真题手把手教学",
+          link: "/designPattern/6",
+        },
+        {
+          text: "创建型：原型模式——谈Prototype无小事",
+          link: "/designPattern/7",
+        },
+        {
+          text: "结构型：装饰器模式——对象装上它，就像开了挂",
+          link: "/designPattern/8",
+        },
+        {
+          text: "结构型：装饰器模式——深入装饰器原理与优秀案例",
+          link: "/designPattern/9",
+        },
+        {
+          text: "结构型：适配器模式——兼容代码就是一把梭",
+          link: "/designPattern/10",
+        },
+        {
+          text: "结构型：代理模式——一家小型婚介所的发家致富之路",
+          link: "/designPattern/11",
+        },
+        {
+          text: "结构型：代理模式——应用实践范例解析",
+          link: "/designPattern/12",
+        },
+        {
+          text: "行为型：策略模式——重构小能手，拆分“胖逻辑”",
+          link: "/designPattern/13",
+        },
+        {
+          text: "行为型：状态模式——自助咖啡机背后的力量",
+          link: "/designPattern/14",
+        },
+        {
+          text: "行为型：观察者模式——鬼故事：产品经理拉了一个钉钉群",
+          link: "/designPattern/15",
+        },
+        {
+          text: "行为型：观察者模式——面试真题手把手教学",
+          link: "/designPattern/16",
+        },
+        { text: "行为型：迭代器模式——真·遍历专家", link: "/designPattern/17" },
+        { text: "前方的路", link: "/designPattern/18" },
+      ],
+    },
+    {
       text: "其他",
       collapsible: true,
       items: [
